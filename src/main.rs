e11fa285838513a14c4202c27d4fb11306f94b0a
apple-yagi <applesryu@gmail.com>
2023-12-24 23:05:04 +0900

    feat: Impl to write char

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use image::{ImageBuffer, Rgba};
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
 
 fn main() {
     let imgx = 200;
@@ -7,8 +9,24 @@ fn main() {
     let mut imgbuf = ImageBuffer::<Rgba<u8>, Vec<u8>>::new(imgx, imgy);
 
     for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = Rgba::<u8>::from([255, 255, 255, 0]);
+        *pixel = Rgba([255u8, 255u8, 255u8, 0]);
     }
+
+    let font = Vec::from(include_bytes!("../fonts/NotoSansJP-Bold.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+
+    let scale = Scale { x: 41.5, y: 200.0 };
+
+    let text = "完全に理解した";
+    draw_text_mut(
+        &mut imgbuf,
+        Rgba([255u8, 255u8, 255u8, 255u8]),
+        0,
+        0,
+        scale,
+        &font,
+        text,
+    );
 
     imgbuf.save("emoji.png").unwrap();
 }
