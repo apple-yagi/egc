08e5b35e130d53a6edf9ead3e7b4d8e98a605570
apple-yagi <applesryu@gmail.com>
2023-12-24 20:26:29 +0900

    feat: 透過pngを作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use image::{ImageBuffer, Rgba};
+
 fn main() {
-    println!("Hello, world!");
+    let imgx = 200;
+    let imgy = 200;
+
+    let mut imgbuf = ImageBuffer::<Rgba<u8>, Vec<u8>>::new(imgx, imgy);
+
+    for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = Rgba::<u8>::from([255, 255, 255, 0]);
+    }
+
+    imgbuf.save("emoji.png").unwrap();
 }
